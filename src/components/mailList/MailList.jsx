d598e63951e2d0748ab21157c8d875eed5ff2ff8
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-05-08 22:04:39 -0700

    first commit

---
+++ src/components/mailList/MailList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./mailList.css";
+
+const MailList = () => {
+  return (
+    <div className="mail">
+      <h1 className="mailTitle">Save time, save Money!</h1>
+      <span className="mailDesc">
+        Sign up and we 'll send the best delas to you
+      </span>
+      <div className="mailInputCotainer">
+        <input type="text" placeholder="Your Email" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
+};
+
+export default MailList;
